670fcf508185e81c539695754f5e5a25ae290c29
Caroline Ferraz <projetos.crrz@gmail.com>
2023-01-30 00:55:06 -0300

    Main

--- Java-Desafio-POO/src/br/com/dio/desafio/Main.java
+++ Java-Desafio-POO/src/br/com/dio/desafio/Main.java
@@ -2,7 +2,9 @@ package br.com.dio.desafio;
 
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -24,10 +26,37 @@ public class Main {
 		mentoria.setDesricao("Mentoria de Java");
 		mentoria.setData(LocalDate.now());
 		
-		System.out.println(curso1);
-		System.out.println(curso2);
-		System.out.println(mentoria);
+		Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+        
+        Dev devCarol = new Dev();
+        devCarol.setNome("Carol");
+        devCarol.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Carol:" + devCarol.getConteudosInscritos());
+        devCarol.progredir();
+        devCarol.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Carol:" + devCarol.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Carol:" + devCarol.getConteudosConcluidos());
+        System.out.println("XP:" + devCarol.calcularTotalXp());
 		
+        System.out.println("---------------------------------------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos João:" + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcularTotalXp());
 		
 	}
 
