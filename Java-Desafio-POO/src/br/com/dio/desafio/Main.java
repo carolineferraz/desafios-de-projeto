9577b4384802fd8ec624710c0a50ccf33e9dbf09
Caroline Ferraz <projetos.crrz@gmail.com>
2023-01-30 00:00:40 -0300

    Criando as classes Curso e Mentoria e abstraindo seus atributos

---
+++ Java-Desafio-POO/src/br/com/dio/desafio/Main.java
@@ -0,0 +1,34 @@
+package br.com.dio.desafio;
+
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Curso curso1 = new Curso();
+		curso1.setTitulo("POO e fundamentos Java");
+		curso1.setDescricao("Aprenda POO em Java com toda a teoria fundamental e muita prática.");
+		curso1.setCargaHorario(20);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("APIs REST com Java e Spring Boot");
+		curso2.setDescricao("Desenvolva suas primeiras APIs em Java utilizando o framework Spring Boot.");
+		curso2.setCargaHorario(20);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("Mentoria de Java");
+		mentoria.setDescricao("Mentoria de Java");
+		mentoria.setData(LocalDate.now());
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria);
+		
+		
+	}
+
+}
