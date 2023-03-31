2b6ffff7dfae16726f31d820bef5d5dd27de891d
Caroline Ferraz <projetos.crrz@gmail.com>
2023-03-31 19:28:34 -0300

    Orange Tech: Padrões de projeto (Singleton, Strategy e Facade) com Java puro

---
+++ OrangeTech-Java-Spring/LabPadroesProjetoJava/src/dio/gof/singleton/SingletonLazy.java
@@ -0,0 +1,25 @@
+package dio.gof.singleton;
+
+/**
+ * Singleton "preguiçoso"
+ * 
+ * @author Caroline
+ *
+ */
+
+public class SingletonLazy {
+	
+	private static SingletonLazy instancia;
+	
+	private SingletonLazy() {
+		super();
+	}
+	
+	public static SingletonLazy getInstancia() {
+		if (instancia == null) {
+			instancia = new SingletonLazy();
+		}
+		return instancia;
+	}
+
+}
